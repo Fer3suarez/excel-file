0dd51fb7f52053a83b60913c4c5b512af16f44a5
Fer3suarez <suarez11fer@gmail.com>
2021-02-10 20:24:32 +0100

    Se crea un fichero excel

---
+++ Prueba-excel/src/ExcelOOXML.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelOOXML {
+
+    private static final Logger LOGGER = Logger.getLogger("mx.com.hash.newexcel.ExcelOOXML");
+
+    public static void main(String[] args) {
+        File archivo = new File("prueba-excel.xlsx");
+
+        // Creamos el libro de trabajo de Excel formato OOXML
+        Workbook workbook = new XSSFWorkbook();
+
+        // La hoja donde pondremos los datos
+        Sheet pagina = workbook.createSheet("Hoja de prueba");
+
+        // Creamos el estilo paga las celdas del encabezado
+        CellStyle style = workbook.createCellStyle();
+        
+        style.setFillForegroundColor(IndexedColors.AQUA.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        String[] titulos = {"Nombre", "Apellidos", "Email", "DNI"};
+        String[] datos   = {"Fernando", "Suárez Menéndez", "suarez11fer@gmail.com", "00000000X"};
+
+        Row fila = pagina.createRow(0);
+        for (int i = 0; i < titulos.length; i++) {
+            Cell celda = fila.createCell(i);
+            celda.setCellStyle(style);
+            celda.setCellValue(titulos[i]);
+        }
+        fila = pagina.createRow(1);
+        for (int i = 0; i < datos.length; i++) {
+            Cell celda = fila.createCell(i);
+            celda.setCellValue(datos[i]);
+        }
+        
+        //Guardar archivo
+        try {
+            FileOutputStream salida = new FileOutputStream(archivo);
+            workbook.write(salida);
+            workbook.close();
+            LOGGER.log(Level.INFO, "Archivo creado existosamente en {0}", archivo.getAbsolutePath());
+        } catch (FileNotFoundException ex) {
+            LOGGER.log(Level.SEVERE, "Archivo no localizable en sistema de archivos");
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, "Error de entrada/salida");
+        }
+    }
+}
