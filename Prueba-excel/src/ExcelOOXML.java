7d7f4953a0bfbac13a32cfe6067d87d080c1c541
Fer3suarez <suarez11fer@gmail.com>
2021-02-10 21:04:07 +0100

    Se elimina la clase readExcel y se implementa todo en la misma clase (crear y leer archivos excels)

--- Prueba-excel/src/ExcelOOXML.java
+++ Prueba-excel/src/ExcelOOXML.java
@@ -1,7 +1,9 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,24 +14,22 @@ import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelOOXML {
 
     private static final Logger LOGGER = Logger.getLogger("mx.com.hash.newexcel.ExcelOOXML");
-
-    public static void main(String[] args) {
-        File archivo = new File("prueba-excel.xlsx");
-
-        // Creamos el libro de trabajo de Excel formato OOXML
-        Workbook workbook = new XSSFWorkbook();
-
-        // La hoja donde pondremos los datos
-        Sheet pagina = workbook.createSheet("Hoja de prueba");
-
-        // Creamos el estilo paga las celdas del encabezado
-        CellStyle style = workbook.createCellStyle();
-        
+    
+    private static File archivo = new File("prueba-excel.xlsx");
+    private static Workbook workbook = new XSSFWorkbook();
+    private static Sheet pagina = workbook.createSheet("Hoja de prueba");
+    private static CellStyle style = workbook.createCellStyle();
+	private static String nombreArchivo = "prueba-excel.xlsx";
+	private static String rutaArchivo = "C:\\Users\\Ferna\\git\\prueba-excel\\" + nombreArchivo;
+    
+    public static void createExcel() {
+    	
         style.setFillForegroundColor(IndexedColors.AQUA.getIndex());
         style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         String[] titulos = {"Nombre", "Apellidos", "Email", "DNI"};
@@ -46,7 +46,6 @@ public class ExcelOOXML {
             Cell celda = fila.createCell(i);
             celda.setCellValue(datos[i]);
         }
-        
         //Guardar archivo
         try {
             FileOutputStream salida = new FileOutputStream(archivo);
@@ -62,5 +61,33 @@ public class ExcelOOXML {
         } catch (IOException ex) {
             LOGGER.log(Level.SEVERE, "Error de entrada/salida");
         }
+    }
+    
+    public static void readExcel() {
+ 
+		try (FileInputStream file = new FileInputStream(new File(rutaArchivo))) {
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+ 
+			Row row;
+			while (rowIterator.hasNext()) {
+				row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				Cell cell;
+				while (cellIterator.hasNext()) {
+					cell = cellIterator.next();
+					System.out.print(cell.getStringCellValue()+" | ");
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+    }
+
+    public static void main(String[] args) {
+    	createExcel();
+    	readExcel();
     }
 }
